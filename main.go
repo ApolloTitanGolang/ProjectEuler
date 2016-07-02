cd4da169ae98d1e582fe6228fde010ea741e6d21
ApolloTitan <mhagspiel@gmail.com>
2016-07-02 10:13:29 +0200

    - problem 7,8 and 9 added - babylonjs added

--- main.go
+++ main.go
@@ -60,6 +60,27 @@ func main() {
 	endtime = time.Now()
 	fmt.Printf("\nDuration: %v", endtime.Sub(starttime))
 
+	fmt.Println("\n\n\nProblem 7 (10001st prime)")
+	fmt.Println("-----------------------------------------------------------------")
+	starttime = time.Now()
+	problem7()
+	endtime = time.Now()
+	fmt.Printf("\nDuration: %v", endtime.Sub(starttime))
+
+	fmt.Println("\n\n\nProblem 8 (Largest product in a series)")
+	fmt.Println("-----------------------------------------------------------------")
+	starttime = time.Now()
+	problem8()
+	endtime = time.Now()
+	fmt.Printf("\nDuration: %v", endtime.Sub(starttime))
+
+	fmt.Println("\n\n\nProblem 9 (Special Pythagorean triplet)")
+	fmt.Println("-----------------------------------------------------------------")
+	starttime = time.Now()
+	problem9()
+	endtime = time.Now()
+	fmt.Printf("\nDuration: %v", endtime.Sub(starttime))
+
 	fmt.Printf("\n\n")
 }
 
@@ -210,6 +231,108 @@ func problem6() {
 }
 
 
+func problem7() {
+	primeList := make([]int, 0, 10001)
+	primeCount := 0
+	number := 1
+	isPrime := true
 
+	// calculate primes
+	for primeCount < 10001 {
+		isPrime = true
+		number++
 
+		for _, prime := range primeList {
+			if (prime != 1) && (number % prime == 0) {
+				isPrime = false
+				break
+			}
+		}
 
+		if isPrime {
+			primeList = append(primeList, number)
+			primeCount++
+			// fmt.Printf("\nPrimelist: %v - PrimeCount: %v", primeList, primeCount)
+		}
+	}
+
+	fmt.Printf("\nThe 10001st prime number: %v", number)
+}
+
+func problem8() {
+
+	// get byte out of the string and convert (cast) it to an int
+	// i, _ := strconv.Atoi(string(stringNumber[0]))
+
+	greatestProduct := 1
+
+	stringNumber :=
+	"73167176531330624919225119674426574742355349194934"+
+	"96983520312774506326239578318016984801869478851843"+
+	"85861560789112949495459501737958331952853208805511"+
+	"12540698747158523863050715693290963295227443043557"+
+	"66896648950445244523161731856403098711121722383113"+
+	"62229893423380308135336276614282806444486645238749"+
+	"30358907296290491560440772390713810515859307960866"+
+	"70172427121883998797908792274921901699720888093776"+
+	"65727333001053367881220235421809751254540594752243"+
+	"52584907711670556013604839586446706324415722155397"+
+	"53697817977846174064955149290862569321978468622482"+
+	"83972241375657056057490261407972968652414535100474"+
+	"82166370484403199890008895243450658541227588666881"+
+	"16427171479924442928230863465674813919123162824586"+
+	"17866458359124566529476545682848912883142607690042"+
+	"24219022671055626321111109370544217506941658960408"+
+	"07198403850962455444362981230987879927244284909188"+
+	"84580156166097919133875499200524063689912560717606"+
+	"05886116467109405077541002256983155200055935729725"+
+	"71636269561882670428252483600823257530420752963450"
+
+	for startValue := 0; startValue <= 987; startValue++ {
+
+		greatestTempProduct := 1
+
+		for valueIndex := startValue; valueIndex <= startValue + 12; valueIndex++ {
+			number, _ := strconv.Atoi(string(stringNumber[valueIndex]))
+			greatestTempProduct = greatestTempProduct * number
+		}
+
+		if greatestTempProduct > greatestProduct {
+			greatestProduct = greatestTempProduct
+		}
+	}
+
+	fmt.Printf("\nThe largest product in a series: %v", greatestProduct)
+}
+
+func problem9() {
+
+	// a^2 + b^2 = c^2 <> a + b + c = 1000
+	// a^2 + b^2 = (1000 - a - b)^2
+	// SQRT(a^2 + b^2) + a + b = 1000
+	a := 1.0
+	b := 1.0
+	c := 1.0
+	found := false;
+
+	for a = 1; a < 1000; a++ {
+		for b = 1; b < 1000; b++ {
+
+			c = math.Sqrt(math.Pow(a, 2) + math.Pow(b, 2))
+
+			if (c + a + b) == 1000 {
+				found = true
+				break
+			}
+		}
+		if found {
+			fmt.Printf("1")
+			break
+		}
+	}
+
+	p := a * b * c
+
+	fmt.Printf("\nTriplet for 1000 is: %v,  %v, %v", a, b, c)
+	fmt.Printf("\nTriplet product: %v", int(p))
+}
